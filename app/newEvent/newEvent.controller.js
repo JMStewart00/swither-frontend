382ddb6e8f20e78507f21104c9323f18b8abec78
JMStewart00 <jmstewart00@gmail.com>
2017-08-06 11:48:15 -0400

    Styling all but finished for version 1

--- app/newEvent/newEvent.controller.js
+++ app/newEvent/newEvent.controller.js
@@ -2,12 +2,6 @@ class newEventController {
     constructor($rootScope, $auth, $http, $state) {
         let ctrl=this;
         ctrl.$rootScope = $rootScope;
-        ctrl.$rootScope.getGroups();
-
-        ctrl.$rootScope.$watch('groups', () => {
-        	console.log('groups changed');
-            ctrl.$rootScope.getGroups();
-        });
 
 }
 
